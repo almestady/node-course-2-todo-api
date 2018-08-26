d4feef3d147c9ae2cfea2d1a82d97829cc8b192e
Wael Almestady <almestady@gmail.com>
2018-08-26 14:23:41 +0300

    Making all the routes private

--- servers/models/todo.js
+++ servers/models/todo.js
@@ -1,22 +1,24 @@
 var mongoose = require('mongoose');
 
-var Todo = mongoose.model('Todo',{
-  text:{
-    type:String,
-    required:true,
-    minlength:1,
-    trim:true
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
-  completed:{
-    type:Boolean,
-    default:false
+  completed: {
+    type: Boolean,
+    default: false
   },
-  completedAt:{
-    type:Number,
-    default:null
+  completedAt: {
+    type: Number,
+    default: null
+  },
+  _creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true
   }
 });
-
-
 
 module.exports = {Todo};
